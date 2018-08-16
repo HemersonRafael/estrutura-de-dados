6750dde5cf38e700f58934df59f06e3d7cf9e9e8
HemersonRafael <rafaelpontes1995@gmail.com>
2018-08-15 23:25:53 -0300

    add files

---
+++ Pratica01/src/candidato.cpp
@@ -0,0 +1,23 @@
+#include "Candidato.h"
+#include <cstring>
+#include <sstream>
+
+
+using namespace std;
+
+Candidato::Candidato(string linha){
+    stringstream stream(linha);
+    stream >> sobrenome >> nome >> nota;
+}
+
+string Candidato::toString(){
+    stringstream stream;
+    stream << sobrenome << " " << nome << " " << nota; 
+    return stream.str();
+}
+
+bool Candidato::igual(string n, string s){
+    
+    if(sobrenome == s && nome == n) return true;
+    else return false;
+}
